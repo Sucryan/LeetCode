814679a6dc80078107c435b2ba29554e97f95a7f
徐煜宸 <104014521+Sucryan@users.noreply.github.com>
2026-03-25 11:01:41 +0800

    w1-add-comment-25

--- week1/leetcode-25.cpp
+++ week1/leetcode-25.cpp
@@ -10,6 +10,11 @@
  */
 class Solution {
 public:
+    // 一個left, 一個right pointer, right會先走k次，放入vector中。
+    // 確定可以的話，先把vector reverse，left再走k次把reverse後的結果塞進去。
+    // 細節: 把vector init宣告在ptr != nullptr 也就是對於left pointer的大loop下，這樣每次走k次後都會重新init
+        // 並且right pointer在走k次的時候如果發現走到null會直接return 當前的head，因為她說後面不滿足k的不用reverse；
+        // 這樣也可以避免left走到null的可能性
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode* ptr = head;
         while(ptr != nullptr) {
